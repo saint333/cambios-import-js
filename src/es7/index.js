2e3573a7b98a6f0e6e1f1b60876e0478dacdeb8a
David Israel <israeldavi0904@gmail.com>
2022-01-03 21:53:29 -0500

    version 7 de js

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+//sirve para validar la existencia de un valor 
+
+let numbers = [1,2,3,4,5,6,7,8,9];
+if (numbers.includes(10)){
+    console.log("si se encuentra");
+}else{
+    console.log("no se encuentra");
+}
+
+//potencia 
+
+let num1 = 4;
+let num2 = 4;
+let potencia = num1 ** num2;
+
+console.log(potencia);
